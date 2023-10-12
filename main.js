ec314371b65fbd07a9a2e0fe54e45b21f0aad9a0
lnsfany <fanylailatunisa21@gmail.com>
2023-10-12 14:51:21 +0700

    Today

---
+++ main.js
@@ -0,0 +1,45 @@
+//console.log("hello world")
+//alert("notifikasi")
+//prompt("Fany Sayang Azka")
+
+//var Promnet = "Coding is easy"
+//console.log(Promnet)   
+//var Promnet = "Coding not easy"
+//console.log(Promnet)
+
+//let Promnet = "Coding is easy"
+//console.log(Promnet)
+//Promnet = "Coding not easy"
+//console.log(Promnet)
+
+//const Promnet = "Coding is easy"
+//console.log(Promnet)
+//Promnet = "Coding not easy"
+//console.log(Promnet) 
+
+//let totalPoin = prompt(" Masukan Poin Anda")
+    //if(totalPoin > 100){
+        //document.write("Congratulation");
+   // }
+   // else{
+       // document.write("<h1>Thank you</h1>");
+    // }
+
+    // let x = 6;
+// let y = 3;
+
+// console.log(x<10 && y>1)
+// console.log(x<10 && y < 1)
+// console.log(x==5||y==5)
+// console.log(x==6||y==5)
+// console.log(!(x==y))
+
+//let p = document.querySelector("p")
+//let button = document.querySelector("button")
+//let input = document.querySelector("input")
+
+//button.addEventListener('click', function(){
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerText = isi
+// })
